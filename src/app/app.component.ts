570abac123bd66d3c4d907340217a0ba7cd4c791
chandukeertipati <131877124+chandukeertipati@users.noreply.github.com>
2024-01-31 15:39:55 +0530

    first File structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Swiftt-Select';
+  constructor(private messageService: MessageService) { }
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success Message', detail: 'Operation successful' });
+  }
+  showInfo() {
+    this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content' });
+  }
+  showWarn() {
+    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: 'Message Content' });
+  }
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Message Content' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { MessageModule } from 'primeng/message';
+import { MessageService } from 'primeng/api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+import { CareersComponent } from './container/careers/careers.component';
+import { LogInComponent } from './shared/log-in/log-in.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CareersComponent,
+    LogInComponent,
+    HeaderComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MessageModule,
+    ToastModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    // FlexLayoutModule,
+    RouterModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
